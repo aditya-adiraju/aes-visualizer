074820597dbc245be8ba9b4c3583640e64713606
aditya-adiraju <adiraju@student.ubc.ca>
2025-01-13 23:37:47 -0800

    Added Circle component in flow chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { MathJaxContext } from 'better-react-mathjax';
 import './App.css'
 import Page from './components/Page';
+import Circle from './components/FlowChart/Circle';
 
 function App() {
     return (
         <MathJaxContext>
-            <Page />
+            <Circle />
         </MathJaxContext>
     )
 }
